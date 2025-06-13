bf59b532dc2413335956702f1e74ddab50cfc486
DESKTOP-GJCA51G\user1 <qbsb147>
2025-06-13 12:32:19 +0900

    test

--- FrontEnd/src/components/list/RegisterList.jsx
+++ FrontEnd/src/components/list/RegisterList.jsx
@@ -11,7 +11,7 @@ const RegisterList = () => {
         <Label>호스트 신청</Label>
       </InputBox>
 
-      <Button type="submit">등록s</Button>
+      <Button type="submit">등록</Button>
     </FormContainer>
   );
 };
