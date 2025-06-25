3923f7bb43ea35f2b1358a84090cd85d65693155
DESKTOP-LTIII62\user1 <ashes999@naver.com>
2025-06-25 15:37:32 +0900

    no message

---
+++ BackEnd/src/main/java/com/minePing/BackEnd/repository/MentalRepositoryImpl.java
@@ -0,0 +1,12 @@
+package com.minePing.BackEnd.repository;
+
+import com.minePing.BackEnd.entity.Mental;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MentalRepositoryImpl implements MentalRepository {
+    @Override
+    public void save(Mental mental) {
+
+    }
+}
