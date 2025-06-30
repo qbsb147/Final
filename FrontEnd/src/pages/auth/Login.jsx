b9fcff2e88bbd586d2c57b86149d65027b477b71
DESKTOP-GJCA51G\user1 <qbsb147>
2025-06-30 19:27:34 +0900

    feat:login 완성 signUp wip

--- FrontEnd/src/pages/auth/Login.jsx
+++ FrontEnd/src/pages/auth/Login.jsx
@@ -7,6 +7,7 @@ import Input from '../../styles/Input';
 import { Link, useNavigate } from 'react-router-dom';
 import useAuthStore from '../../store/authStore';
 import memberService from '../../api/members';
+import jwt_decode from 'jwt-decode';
 
 const Login = () => {
   const [userId, setUserId] = useState('');
@@ -18,7 +19,12 @@ const Login = () => {
     e.preventDefault();
     try {
       const member = await memberService.login({ userId, userPwd });
+      const { token } = member;
       console.log('로그인 응답 member:', member);
+      console.log('로그인 응답 token:', token);
+      const decodedToken = jwt_decode(token);
+      console.log('로그인 응답 decodedToken:', decodedToken);
+
       setUser(member);
       alert('로그인 성공!');
       navigate('/');
