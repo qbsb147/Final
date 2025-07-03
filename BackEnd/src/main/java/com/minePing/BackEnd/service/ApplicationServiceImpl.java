7ff9888fce0e4a618f2c2436d9cf00a70baed8cb
DESKTOP-LTIII62\user1 <ashes999@naver.com>
2025-07-03 17:44:56 +0900

    feat : commit 오류 수정

--- BackEnd/src/main/java/com/minePing/BackEnd/service/ApplicationServiceImpl.java
+++ BackEnd/src/main/java/com/minePing/BackEnd/service/ApplicationServiceImpl.java
@@ -94,21 +94,5 @@ public class ApplicationServiceImpl implements ApplicationService {
                 .map(ApplicationDto.ApplicationResponseDto::fromEntity)
                 .toList();
 
-    @Transactional
-    public List<ApplicationDto.ApplicationResponseDto> getReserved() {
-        return applicationRepository.findReservedApplications(LocalDate.now())
-            .stream()
-            .map(ApplicationDto.ApplicationResponseDto::fromEntity)
-            .toList();
-    }
-
-    @Override
-    @Transactional
-    public List<ApplicationDto.ApplicationResponseDto> getUsed() {
-        return applicationRepository.findUsedApplications(LocalDate.now())
-            .stream()
-            .map(ApplicationDto.ApplicationResponseDto::fromEntity)
-            .toList();
-
     }
 }
