d0cf9bb365a0574b9b2f69bfd04ddf9a9fabd25d
DESKTOP-LTIII62\user1 <ashes999@naver.com>
2025-07-04 18:54:49 +0900

    feat : 오류 처리

--- FrontEnd/src/api/application.js
+++ FrontEnd/src/api/application.js
@@ -6,18 +6,17 @@ export const applicationService = {
     const response = await api.post(API_ENDPOINTS.APPLICATION.CREATE);
     return response.data;
   },
-reserved_worcation: async (worcation_no) => {
-  const response = await api.get(API_ENDPOINTS.APPLICATION.RESERVED_WORCATION(worcation_no));
-  return response.data;
-};
-  //예약 목록 조회
+  reserved_worcation: async (worcation_no) => {
+    const response = await api.get(API_ENDPOINTS.APPLICATION.RESERVED_WORCATION(worcation_no));
+    return response.data;
+  },
+  // 예약 목록 조회
   reserved: async (user_no) => {
     const response = await api.get(API_ENDPOINTS.APPLICATION.RESERVED, {
       params: { user_no },
     });
     return response.data;
   },
-
   // 이용완료 목록 조회
   used: async (user_no) => {
     const response = await api.get(API_ENDPOINTS.APPLICATION.USED, {
