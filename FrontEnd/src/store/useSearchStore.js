add7dfcdea44fbf88fc19621730df33229a1c843
MOJIO <mohae@kakao.com>
2025-07-07 21:28:53 +0900

    refactor: Search 관련 상태 관리 스토어를 worcation.js > useSearchStore.js로 변경 - 기존 worcation.js 삭제 및 잘못 import된 파일 수정 (worcationStore > useWorcationStore)  WIP

---
+++ FrontEnd/src/store/useSearchStore.js
@@ -0,0 +1,18 @@
+import { create } from 'zustand';
+
+//키워드 검색
+const useSearchStore = create((set) => ({
+  isValidate: false,
+  setIsValidate: (value) => set({ isValidate: value }),
+  isNonNull: false,
+  setIsNonNull: (value) => set({ isValidate: value }),
+  keyword: '',
+  setKeyword: (value) => set({ keyword: value }),
+  startDate: null,
+  endDate: null,
+  setDates: (start, end) => set({ startDate: start, endDate: end }),
+  popularKeywords: [],
+  setPopularKeywords: (arr) => set({ popularKeywords: arr }),
+}));
+
+export default useSearchStore;
